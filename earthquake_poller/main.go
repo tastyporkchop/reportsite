f229779e58c90f208ef282212a551946865cb5ca
Angus Campbell <angus.b.e.campbell@gmail.com>
2015-12-27 08:09:02 -0800

    broke out sql functions. added test file.

--- earthquake_poller/main.go
+++ earthquake_poller/main.go
@@ -61,34 +61,64 @@ func main() {
 	}
 	//log.Print(spew.Sdump(feed))
 
+	//
+	tx, err := db.Begin()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer tx.Rollback()
+
 	for i := range feed.Entry {
-		err := processEntry(feed.Entry[i], db)
+		err := processEntry(feed.Entry[i], tx)
 		if err != nil {
 			log.Printf("Trouble processing entry: %s", err)
 		}
 	}
+
+	err = tx.Commit()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
-func processEntry(entry *usgsatom.USGSEntry, db *sql.DB) error {
-	tx, err := db.Begin()
+//
+func entryExists(entry *usgsatom.USGSEntry, tx *sql.Tx) bool {
+	// check if event exists
+	stmt, err := tx.Prepare("SELECT COUNT(*) from earthquake_event where event_id=$1")
 	if err != nil {
-		return err
+		log.Fatal(err)
 	}
-	defer tx.Rollback()
+	defer stmt.Close()
 
+	var idcount int
+	err = stmt.QueryRow(entry.ID).Scan(&idcount)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return idcount > 0
+}
+
+//
+func insertEntry(entry *usgsatom.USGSEntry, tx *sql.Tx) error {
+	// insert the record
 	stmt, err := tx.Prepare("INSERT INTO earthquake_event (event_id, title, updated, link, summary, summary_type, point, elevation) VALUES($1, $2, $3, $4, $5, $6, $7, $8)")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer stmt.Close()
 
-	_, err := stmt.Exec(entry.ID, entry.Title, entry.Updated, entry.Link[0], entry.Summary.Body, entry.Summary.Type, entry.Point, entry.Elev)
+	_, err = stmt.Exec(entry.ID, entry.Title, string(entry.Updated), entry.Link[0].Href, entry.Summary.Body, entry.Summary.Type, entry.Point, entry.Elev)
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = tx.Commit()
-	if err != nil {
-		return err
-	}
 	return nil
+}
+
+//
+func processEntry(entry *usgsatom.USGSEntry, tx *sql.Tx) error {
+
+	if entryExists(entry, tx) {
+		return nil
+	}
+	return insertEntry(entry, tx)
 }
---
+++ earthquake_poller/main_test.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestParseUSGSAtom(t *testing.T) {
+
+}
